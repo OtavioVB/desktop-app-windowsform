9c05ae1ac48eda92eea4108598f7d6f02cc19fd5
Otávio <101956836+OtavioVB@users.noreply.github.com>
2022-06-19 19:32:45 -0300

    FALHA DE CONEXÃO

--- Login_Panel/Login_Panel/Banco/Conexão.cs
+++ Login_Panel/Login_Panel/Banco/Conexão.cs
@@ -7,14 +7,22 @@ namespace Login_Panel.Banco
         protected static MySqlConnection? CONEXAO;
         protected static MySqlConnection CONEXAO_BANCO()
         {
-            CONEXAO = new MySqlConnection(
-                "server='HOST_SERVER_NAME';" +
-                "database='DATABASE_NAME';" +
-                "uid='DATABASE_USERNAME';" +
-                "pwd='DATABASE_PASSWORD';" +
-                "port='DATABASE_PORT_DEFAULT=3306'");
-            CONEXAO.Open();
-            return CONEXAO;
+            try{
+                CONEXAO = new MySqlConnection(
+                    "server='HOST_SERVER_NAME';" +
+                    "database='DATABASE_NAME';" +
+                    "uid='DATABASE_USERNAME';" +
+                    "pwd='DATABASE_PASSWORD';" +
+                    "port='DATABASE_PORT_DEFAULT=3306'");
+                CONEXAO.Open();
+                return CONEXAO;
+            }
+            catch
+            {
+                throw;
+                MessageBox.Show("Não há conexão com a internet, o aplicativo será encerrado, tente novamente mais tarde. Caso o problema persistir, entre em contato com a equipe de suporte.", "Tente novamente, mais tarde", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Exit();
+            }
         }
     }
 }
